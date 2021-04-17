d7a13a6e0ee9a31264c473c0505a5f22aed0d2ee
Abhinav Vadrevu <abhinavvadrevu1@gmail.com>
2021-04-17 16:50:23 -0700

    changes to test learnings from shakeel

--- loss_drop_experiment/compute_loss.py
+++ loss_drop_experiment/compute_loss.py
@@ -6,16 +6,22 @@ print("importing complete")
 with open('loss_drop_experiment/download_links.csv') as csvfile:
   csv_reader = csv.reader(csvfile)
   header = next(csv_reader)
+  print(header)
   for row in csv_reader:
     [filename, _, fileid, _, _, _] = row
+    if 'tier1' in filename:
+      continue
+    print((filename, fileid))
     # Download the weights
     dest_path = './loss_drop_experiment/weights/%s' % filename
     gdd.download_file_from_google_drive(file_id=fileid,
                                     dest_path=dest_path,
                                     unzip=False)
+    print("download completed")
 
     # Compute the loss
     loss = get_checkpoint_loss(dest_path)
+    print("loss computed")
     print((filename, fileid))
     print(loss)
     print('')
--- loss_drop_experiment/get_loss_from_checkpoint.py
+++ loss_drop_experiment/get_loss_from_checkpoint.py
@@ -4,23 +4,42 @@ from model.tier import Tier
 from model.tts import TTS
 import torch
 import torch.nn as nn
+from utils.hparams import HParam
+from utils.constant import f_div, t_div
 
 
 
 # INPUT
 def get_checkpoint_loss(chkpt_path):
+  config_path = 'config/blizzard.yaml'
   tier = int(chkpt_path.split('tier')[1].split('_')[0]) # Yes it's hacky, sorry
+  print('tier: %d' % tier)
   checkpoint = torch.load(chkpt_path)
-  hp = checkpoint['hp_str']
+  print("Checkpoint loaded")
+  hp = HParam(config_path)
+  with open(config_path, 'r') as f:
+      model_hp = checkpoint['hp_str']
+      hp_str = ''.join(f.readlines())
+      if model_hp != hp_str:
+        print(model_hp)
+        print('')
+        print(hp_str)
+        print("ERROR: ISSUE WITH DIFFERENT HPs")
   model = get_model(tier, hp)
+  print("Got model")
   model.load_state_dict(checkpoint['model'])
+  print("Model loaded")
   optimizer = torch.optim.Adam(
       model.parameters(), 
       lr=hp.train.adam.lr
   )
+  print("Got optimizer")
   args = get_args(tier)
+  print("Got args")
   testloader = get_testloader(hp, args)
+  print("Got testloader")
   loss = compute_loss(args, model, testloader, criterion)
+  print("Got loss")
   return loss
 
 def get_args(tier):
@@ -63,7 +82,8 @@ def get_model(tier, hp):
             layers=hp.model.layers[tier-1],
             tierN=tier
         )
-    model = nn.DataParallel(model).cuda()
+    # model = nn.DataParallel(model).cuda()
+    model = model.cuda()
     return model
 
 def get_testloader(hp, args):
@@ -71,12 +91,14 @@ def get_testloader(hp, args):
 
 def compute_loss(args, model, testloader, criterion):
     model.eval()
+    print("model evaled")
     # torch.backends.cudnn.benchmark = False
 
     test_loss = []
     loader = tqdm(testloader, desc='Testing is in progress', dynamic_ncols=True)
     with torch.no_grad():
         for input_tuple in loader:
+            print("loading inputs")
             if args.tts:
                 seq, text_lengths, source, target, audio_lengths = input_tuple
                 mu, std, pi, alignment = model(
@@ -91,11 +113,13 @@ def compute_loss(args, model, testloader, criterion):
                     source.cuda(non_blocking=True),
                     audio_lengths.cuda(non_blocking=True)
                 )
+            print("computing things")
             loss = criterion(
                 target.cuda(non_blocking=True),
                 mu, std, pi,
                 audio_lengths.cuda(non_blocking=True)
             )
+            print("computed loss")
             test_loss.append(loss)
 
         test_loss = sum(test_loss) / len(test_loss)
