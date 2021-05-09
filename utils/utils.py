18f5f817e2f4250dc132cddd9dd80b1e73ba31f3
Abhinav Vadrevu <abhinavvadrevu1@gmail.com>
2021-05-08 21:24:58 -0700

    changes for training new dataset

--- utils/utils.py
+++ utils/utils.py
@@ -18,6 +18,23 @@ def get_length(wavpath, sample_rate):
     audio = audiosegment.from_file(wavpath).resample(sample_rate_Hz=sample_rate)
     return audio.duration_seconds
 
+def remove_punc(text):
+    return text \
+    .replace(',','') \
+    .replace('.','') \
+    .replace('?','') \
+    .replace('*','') \
+    .replace(')','') \
+    .replace('(','') \
+    .replace('[','') \
+    .replace(']','') \
+    .replace('/','') \
+    .replace('{','') \
+    .replace('}','') \
+    .replace(';','') \
+    .replace(':','') \
+    .replace('&','')
+
 def process_blizzard(text: str):
     original_text = text
     text = text.replace('@ ', '').replace('# ', '').replace('| ', '')
@@ -28,38 +45,104 @@ def process_blizzard(text: str):
     text = text.replace('"', '')
     text = text.replace(']', '')
     text = text.replace('[', '')
+    text = text.replace('/', '')
+    text = text.replace('}', '')
+    text = text.replace('{', '')
+    text = text.replace('*', '')
+    text = text.replace('<c', '')
     text = text.replace('&', ' and ')
     text = text.replace('%', ' percent ')
     text = text.replace('$', ' dollars ')
 
 
-    no_punc_text = text.replace(',','').replace('.','')
-    easy_numbers = [int(i) for i in no_punc_text.split() if i.isdigit()]
+    no_punc_text = text.replace(',','').replace('.','').replace('?','')
+    easy_numbers = [
+        (int(remove_punc(i)), i)
+        for i in text.split()
+        if remove_punc(i).isdigit()
+    ]
     # Now find and replace ordinal numbers
     ordinal_number_strs = [
+        (int(remove_punc(i).replace('st','').replace('th','').replace('rd','').replace('nd','')), i)
+        for i in text.split() if
+        (
+            remove_punc(i).endswith('st') or
+            remove_punc(i).endswith('th') or
+            remove_punc(i).endswith('rd') or
+            remove_punc(i).endswith('nd')
+        ) and
+        (
+            remove_punc(i) \
+            .replace('st','') \
+            .replace('th','') \
+            .replace('rd','') \
+            .replace('nd','') \
+            .isdigit())
+    ]
+    degrees_number_strs = [
         i for i in no_punc_text.split() if
-        (i.endswith('st') or i.endswith('th') or i.endswith('rd') or i.endswith('nd')) and
-        (i.replace('st','').replace('th','').replace('rd','').replace('nd','').isdigit())
+        (i.endswith('deg') or i.endswith('d')) and
+        i.replace('deg','').replace('d','').isdigit()
+    ]
+
+    pounds_number_strs = [
+        (int(remove_punc(i).replace('L','')), i)
+        for i in text.split() if
+        (i.endswith('L') or i.startswith('L')) and
+        remove_punc(i).replace('L','').isdigit()
     ]
 
     easy_numbers = sorted(easy_numbers, key=lambda x: -1*len(str(x)))
     ordinal_number_strs = sorted(ordinal_number_strs, key=lambda x: -1*len(str(x)))
+    degrees_number_strs = sorted(degrees_number_strs, key=lambda x: -1*len(str(x)))
+    pounds_number_strs = sorted(pounds_number_strs, key=lambda x: -1*len(str(x)))
 
-    for number in easy_numbers:
+    for number, number_str in easy_numbers:
         number_text = inflect_engine.number_to_words(number)
         if number > 1000 and number < 2200:
             number_text = inflect_engine.number_to_words(number, group=2)
         number_text = number_text.replace(',', '')
         number_text = ' ' + number_text.replace('-', ' ') + ' '
-        text = text.replace(str(number), number_text)
+        text = text.replace(number_str, number_text)
 
-    for ordinal_number in ordinal_number_strs:
-        number = int(ordinal_number.replace('st','').replace('th','').replace('rd','').replace('nd',''))
-        number_text = num2words(number, to='ordinal')
+        # print(txt_filepath)
+        # print(original_text)
+        # print(text)
+        # print('')
+
+    for ordinal_number, ordinal_number_str in ordinal_number_strs:
+        number_text = num2words(ordinal_number, to='ordinal')
         number_text = number_text.replace(',', '')
         number_text = ' ' + number_text.replace('-', ' ') + ' '
-        text = text.replace(ordinal_number, str(number))
-        text = text.replace(str(number), number_text)
+        text = text.replace(ordinal_number_str, number_text)
+
+        # print(txt_filepath)
+        # print(original_text)
+        # print(text)
+        # print('')
+
+    for degree_number in degrees_number_strs:
+        number = int(degree_number.replace('deg','').replace('d',''))
+        number_text = inflect_engine.number_to_words(number)
+        number_text = number_text.replace(',', '')
+        number_text = ' ' + number_text.replace('-', ' ') + ' '
+        text = text.replace(str(degree_number), number_text + ' degrees')
+
+        # print(txt_filepath)
+        # print(original_text)
+        # print(text)
+        # print('')
+
+    for pound_number, pount_str in pounds_number_strs:
+        number_text = inflect_engine.number_to_words(pound_number)
+        number_text = number_text.replace(',', '')
+        number_text = ' ' + number_text.replace('-', ' ') + ' '
+        text = text.replace(pount_str, number_text + ' pounds')
+
+        # print(txt_filepath)
+        # print(original_text)
+        # print(text)
+        # print('')
 
     text = text.replace('  ', ' ')
     text = text.replace(' ,', ',')
