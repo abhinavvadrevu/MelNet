5f39dc57948a8cabffef054777c58e4ed47affe9
Abhinav Vadrevu <abhinavvadrevu1@gmail.com>
2021-05-17 21:50:26 -0700

    added new cleanup script

--- scripts/clean_up_data.py
+++ scripts/clean_up_data.py
@@ -6,6 +6,7 @@ import os
 import shutil
 from tqdm import tqdm
 import audiosegment
+import json
 # Existing directory structure:
 # - datasets
 #   - segmented_training_data
@@ -22,81 +23,131 @@ import audiosegment
 #   - train_txt
 #   - test_wav
 #   - test_txt
-LAST_INDEX = None
 
-def get_last_index(train=False):
-  global LAST_INDEX
-  if LAST_INDEX != None:
-    return LAST_INDEX
-  path = 'datasets/train_txt'
-  if not train:
-    path = 'datasets/test_txt'
-  file_list = glob.glob(
-      os.path.join(path,'**', '*.txt'),
-      recursive=True
-  )
-  last_index = 0
-  # Assumes we always have 6 digits in naming
-  if len(file_list) > 0:
-    last_index = max(map(lambda x: int(x[-10:-4]), file_list))
-  LAST_INDEX = last_index
-  return last_index
-
-def save_new_file(wavpath, sentence, train=False):
-  # saves a new file into our new dataset
-  # First get last index number
-  global LAST_INDEX
-  last_index = get_last_index(train)
-  next_index = last_index + 1
-  new_index_str = f'{next_index:06}'
-  train_str = 'train' if train else 'test'
-  txt_filepath = 'datasets/%s_txt/%s.txt' % (train_str, new_index_str)
-  wav_filepath = 'datasets/%s_wav/%s.wav' % (train_str, new_index_str)
-  # First save the txt in the right place
-  write_txt_file(txt_filepath, sentence)
-  # now copy the wav file into the right place
-  old_wav_file_name = os.path.basename(wavpath)
-  new_wav_file_path = os.path.dirname(wav_filepath)
-  shutil.copy(wavpath, new_wav_file_path)
-  os.rename(os.path.join(new_wav_file_path, old_wav_file_name), wav_filepath)
-  LAST_INDEX += 1
+def process_blizzard(text: str, txt_filepath):
+    original_text = text
+    text = text.replace('@ ', '').replace('# ', '').replace('| ', '')
+    # THE FOLLOWING LINE ONLY EXISTS FOR CLEANED BLIZZARD
+    # BECAUSE I'M TRYING TO REUSE WEIGHTS FROM EXISTIN DATA WHICH DIDN'T
+    # HAVE QUOTES. EVENTUALLY, I DO NEED TO FIND A WAY TO SUPPORT THIS
+    # SYMBOL AS IT'S A CRITICAL ONE IN DEFINING HOW SPEECH SOUNDS
+    text = text.replace('"', '')
+    text = text.replace(']', '')
+    text = text.replace('[', '')
+    text = text.replace('/', '')
+    text = text.replace('}', '')
+    text = text.replace('{', '')
+    text = text.replace('*', '')
+    text = text.replace('<c', '')
+    text = text.replace('&', ' and ')
+    text = text.replace('%', ' percent ')
+    text = text.replace('$', ' dollars ')
 
 
-def get_length(wavpath, sample_rate):
+    no_punc_text = text.replace(',','').replace('.','').replace('?','')
+    easy_numbers = [
+        (int(remove_punc(i)), i)
+        for i in text.split()
+        if remove_punc(i).isdigit()
+    ]
+    # Now find and replace ordinal numbers
+    ordinal_number_strs = [
+        (int(remove_punc(i).replace('st','').replace('th','').replace('rd','').replace('nd','')), i)
+        for i in text.split() if
+        (
+            remove_punc(i).endswith('st') or
+            remove_punc(i).endswith('th') or
+            remove_punc(i).endswith('rd') or
+            remove_punc(i).endswith('nd')
+        ) and
+        (
+            remove_punc(i) \
+            .replace('st','') \
+            .replace('th','') \
+            .replace('rd','') \
+            .replace('nd','') \
+            .isdigit())
+    ]
+    degrees_number_strs = [
+        i for i in no_punc_text.split() if
+        (i.endswith('deg') or i.endswith('d')) and
+        i.replace('deg','').replace('d','').isdigit()
+    ]
+
+    pounds_number_strs = [
+        (int(remove_punc(i).replace('L','')), i)
+        for i in text.split() if
+        (i.endswith('L') or i.startswith('L')) and
+        remove_punc(i).replace('L','').isdigit()
+    ]
+
+    easy_numbers = sorted(easy_numbers, key=lambda x: -1*len(str(x)))
+    ordinal_number_strs = sorted(ordinal_number_strs, key=lambda x: -1*len(str(x)))
+    degrees_number_strs = sorted(degrees_number_strs, key=lambda x: -1*len(str(x)))
+    pounds_number_strs = sorted(pounds_number_strs, key=lambda x: -1*len(str(x)))
+
+    for number, number_str in easy_numbers:
+        number_text = inflect_engine.number_to_words(number)
+        if number > 1000 and number < 2200:
+            number_text = inflect_engine.number_to_words(number, group=2)
+        number_text = number_text.replace(',', '')
+        number_text = ' ' + number_text.replace('-', ' ') + ' '
+        text = text.replace(number_str, number_text)
+
+    for ordinal_number, ordinal_number_str in ordinal_number_strs:
+        number_text = num2words(ordinal_number, to='ordinal')
+        number_text = number_text.replace(',', '')
+        number_text = ' ' + number_text.replace('-', ' ') + ' '
+        text = text.replace(ordinal_number_str, number_text)
+
+    for degree_number in degrees_number_strs:
+        number = int(degree_number.replace('deg','').replace('d',''))
+        number_text = inflect_engine.number_to_words(number)
+        number_text = number_text.replace(',', '')
+        number_text = ' ' + number_text.replace('-', ' ') + ' '
+        text = text.replace(str(degree_number), number_text + ' degrees')
+
+    for pound_number, pount_str in pounds_number_strs:
+        number_text = inflect_engine.number_to_words(pound_number)
+        number_text = number_text.replace(',', '')
+        number_text = ' ' + number_text.replace('-', ' ') + ' '
+        text = text.replace(pount_str, number_text + ' pounds')
+
+    text = text.replace('  ', ' ')
+    text = text.replace(' ,', ',')
+    text = text.replace(' .', '.')
+    text = text.replace(' !', '!')
+    text = text + EOS
+    
+    seq = None
+    try:
+        seq = [_symbol_to_id[c] for c in text]
+    except Exception as e:
+        print(txt_filepath)
+        print(original_text)
+        print(text)
+        print('')
+        return False
+    return True
+
+def save_new_sentence(original_sentence, parsed_sentence, wav_path, wav_length, train=True):
+  to_save = {
+    'original_sentence': original_sentence,
+    'parsed_sentence': parsed_sentence,
+    'wav_path': wav_path,
+    'wav_length': wav_length,
+    'train': train,
+  }
+  with open('./datasets/blizzard.json', 'r+') as json_file:
+    data = json.load(json_file)
+    data.append(to_save)
+    json_file.seek(0)
+    json.dump(data, json_file)
+
+
+def get_length(wavpath, sample_rate=22050):
     audio = audiosegment.from_file(wavpath).resample(sample_rate_Hz=sample_rate)
     return audio.duration_seconds
-
-def parse_segmented():
-  # parses segmented_training_data
-  # splits into train and test based on existing split
-  SAMPLE_RATE = 22050
-  MAX_DURATION = 10.0 # Max duration 10 seconds for now
-  dataset = []
-  train_dataset = []
-  test_dataset = []
-  root_dir = 'datasets/segmented_training_data'
-  with open(os.path.join(root_dir, 'prompts.gui'), 'r') as f:
-    lines = f.read().splitlines()
-    filenames = lines[::3]
-    sentences = lines[1::3]
-    for filename, sentence in tqdm(zip(filenames, sentences), total=len(filenames)):
-      wav_path = os.path.join(root_dir, 'wavn', filename + '.wav')
-      length = get_length(wav_path, SAMPLE_RATE)
-      if length < MAX_DURATION:
-        sentence = sentence.replace('@ ', '').replace('# ', '').replace('| ', '')
-        dataset.append((wav_path, sentence))
-
-  random.seed(123)
-  random.shuffle(dataset)
-  train_dataset = dataset[:int(0.95 * len(dataset))]
-  test_dataset = dataset[int(0.95 * len(dataset)):]
-
-  for wavpath, sentence in train_dataset:
-    save_new_file(wavpath, sentence, True)
-  for wavpath, sentence in test_dataset:
-    save_new_file(wavpath, sentence, False)
-
-  return
 
 def read_txt_file(filepath):
   with open(filepath, 'r') as reader:
@@ -108,31 +159,6 @@ def write_txt_file(filepath, sentence):
   f.write(sentence)
   f.close()
   return sentence
-
-list_of_sentences = set()
-def sentence_exists(new_sentence):
-  global list_of_sentences
-  # checks if a given sentence already exists in train/test set
-  file_list = glob.glob(
-      os.path.join('datasets/train_txt','*'),
-      recursive=True
-  ) + glob.glob(
-      os.path.join('datasets/test_txt','*'),
-      recursive=True
-  )
-  if len(file_list) == 0:
-    return False
-  if len(list_of_sentences) == 0: # TODO: Update this memoized list as we add new sentences in
-    sentences = []
-    for filepath in file_list:
-      sentence = read_txt_file(filepath)
-      sentences.append(sentence.replace(" ", "").lower())
-    list_of_sentences = set(sentences)
-
-  if new_sentence.replace(" ", "").lower() in list_of_sentences:
-    print("Sentence exists! Tried adding \"%s\" to dataset" % new_sentence)
-    return True
-  return False
 
 def parse_new_data(txt_path, wav_path):
   # reads new data
@@ -151,23 +177,19 @@ def parse_new_data(txt_path, wav_path):
 
   data_to_save = []
   i = 0
-  for txt_file in txt_file_list:
+  for txt_file in tqdm(txt_file_list, desc="Processing data files"):
     i += 1
     # first check if sentence is already in the dataset
-    print("\n\nProcessing file %d: %s" % (i, txt_file))
+    # print("\n\nProcessing file %d: %s" % (i, txt_file))
     sentence = read_txt_file(txt_file)
-    if sentence_exists(sentence):
-      continue
     txt_filename = os.path.basename(txt_file)
     wav_filename = os.path.basename(txt_file).replace('.txt', '.wav')
-    for wav_file in wav_file_list:
-      if wav_file.endswith(wav_filename):
-        # print("Found matching wav file! %s" % wav_filename)
-        # data_to_save.append((txt_file, wav_file))
-        if random.random() < 0.05:
-          save_new_file(wav_file, read_txt_file(txt_file), False)
-        else:
-          save_new_file(wav_file, read_txt_file(txt_file), True)
+    wav_file = txt_file.replace('txt', 'wav')
+    wav_length = get_length(wav_file)
+    processed_sentence = process_blizzard(sentence, txt_file)
+    # Save txt file with length of audio AND processed sentence
+    train = False if random.random() < 0.05 else True
+    save_new_sentence(sentence, processed_sentence, wav_file, wav_length, train=True)
 
 def compress():
   # creates compressed version of data at 15000 SR to help with memory
