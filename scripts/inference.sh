90087222e44c7b17f050995e1be9dd8af6f93b51
Abhinav Vadrevu <abhinavvadrevu1@gmail.com>
2021-06-04 14:10:42 -0700

    work on testing tier 1

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -1,6 +1,6 @@
-curl https://melnet-training-runs.s3.amazonaws.com/v5/t1/chkpt/blizzard-alldata-v5/blizzard-alldata-v5_f3690cd_tier1_007.pt -O
+curl https://melnet-training-runs.s3.amazonaws.com/v5/t1/chkpt/blizzard-alldata-v5/blizzard-alldata-v5_f3690cd_tier1_012.pt -O
 curl https://melnet-training-runs.s3.amazonaws.com/v5/t2/chkpt/blizzard-alldata-v5/blizzard-alldata-v5_f203de9_tier2_018.pt -O
 curl https://melnet-training-runs.s3.amazonaws.com/v5/t3/chkpt/blizzard-alldata-v5/blizzard-alldata-v5_f203de9_tier3_012.pt -O
 curl https://melnet-training-runs.s3.amazonaws.com/v5/t4/chkpt/blizzard-alldata-v5/blizzard-alldata-v5_f203de9_tier4_008.pt -O
-curl https://melnet-training-runs.s3.amazonaws.com/v5/t5/chkpt/blizzard-alldata-v5/blizzard-alldata-v5_t5_f203de9_tier5_001.pt -O
+curl https://melnet-training-runs.s3.amazonaws.com/v5/t5/chkpt/blizzard-alldata-v5/blizzard-alldata-v5_t5_f203de9_tier5_002.pt -O
 curl https://melnet-training-runs.s3.amazonaws.com/v5/t6/chkpt/blizzard-alldata-v5/blizzard-alldata-v5_f203de9_tier6_002.pt -O
